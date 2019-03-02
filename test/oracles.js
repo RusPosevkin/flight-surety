cb53a14205ccb8972615f53933cd45075a20644d
Ruslan Posevkin <rus.posevkin@gmail.com>
2019-03-02 15:06:19 +0300

    fix oracles tests

--- test/oracles.js
+++ test/oracles.js
@@ -1,9 +1,9 @@
+
 var Test = require('../config/testConfig.js');
 //var BigNumber = require('bignumber.js');
 
 contract('Oracles', async (accounts) => {
 
-  const TEST_ORACLES_COUNT = 20;
   const STATUS_CODE_UNKNOWN = 0;
   const STATUS_CODE_ON_TIME = 10;
   const STATUS_CODE_LATE_AIRLINE = 20;
@@ -26,7 +26,7 @@ contract('Oracles', async (accounts) => {
     let fee = await config.flightSuretyApp.REGISTRATION_FEE.call();
 
     // ACT
-    for(let a=1; a<TEST_ORACLES_COUNT; a++) {      
+    for(let a = 1; a < 10; a++) {      
       await config.flightSuretyApp.registerOracle({ from: accounts[a], value: fee });
       let result = await config.flightSuretyApp.getMyIndexes.call({from: accounts[a]});
       console.log(`Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}`);
@@ -47,7 +47,7 @@ contract('Oracles', async (accounts) => {
     // loop through all the accounts and for each account, all its Indexes (indices?)
     // and submit a response. The contract will reject a submission if it was
     // not requested so while sub-optimal, it's a good test of that feature
-    for(let a=1; a<TEST_ORACLES_COUNT; a++) {
+    for(let a = 1; a < 10; a++) {
 
       // Get oracle information
       let oracleIndexes = await config.flightSuretyApp.getMyIndexes.call({ from: accounts[a]});
@@ -60,7 +60,7 @@ contract('Oracles', async (accounts) => {
         }
         catch(e) {
           // Enable this when debugging
-           console.log('\nError', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
+           console.log(idx, oracleIndexes[idx].toNumber(), flight, timestamp);
         }
 
       }
